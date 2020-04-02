92acb5b9eabce0c41aa28dc1a1ae9011837b2e5f
yangxrgit <467636649@qq.com>
2020-04-02 19:30:01 +0800

    chapter10-3

--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController //表明是被扫描的类
 public class MyGetMethod {
 
+
+    //给客服端返回cookie的方法
     @RequestMapping(value = "/getCookies",method = RequestMethod.GET) //设定访问路径、访问方法
     public String getCookies(HttpServletResponse response){
 
@@ -22,4 +25,15 @@ public class MyGetMethod {
 
         return "恭喜你获得cookies信息成功";
     }
+
+    //要求客户端携带cookies访问
+    @RequestMapping(value = "/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+
+
+        return "你必须携带cookies信息来";
+    }
+
 }
